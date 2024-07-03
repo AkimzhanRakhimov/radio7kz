2a79729349d6015db0b407303ff1eb5b9571abf6
AkimzhanRakhimov <rakhimovakimzhan@gmail.com>
2024-07-04 04:46:20 +0500

    Started work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>Рекламное агенство Arna multimedia</title>  
+        <title>Радио 7 Семей/title>  
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">    
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
